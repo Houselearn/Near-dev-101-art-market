b07ba30a19340b78cbb179fccc99ad106549099c
codename <111389730+Houselearn@users.noreply.github.com>
2022-08-16 22:00:35 +0100

    Delete cArt Market directory

--- cArt Market/components/loader/loader.tsx
+++
@@ -1,9 +0,0 @@
-import badge from 'lib/assets/badge.svg';
-
-export function Loader() {
-  return (
-    <div className="flex items-center justify-center h-64">
-      <img src={badge.src} className="animate-bounce h-12" />
-    </div>
-  )
-}
